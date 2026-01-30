9ad18d2a5fadf84a84c284b986af7e0273349d5a
ryuya <ryuya1adgjmptw@gmail.com>
2026-01-30 22:23:04 +0900

    watch対応を始めてみた

---
+++ MochiLog Watch App Watch App/MochiLog_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MochiLog_Watch_AppApp.swift
+//  MochiLog Watch App Watch App
+//
+//  Created by りゅうや on 2026/01/30.
+//
+
+import SwiftUI
+
+@main
+struct MochiLog_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MochiLog Watch App/MochiLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MochiLogApp.swift
+//  MochiLog Watch App
+//
+//  Created by りゅうや on 2026/01/30.
+//
+
+import SwiftUI
+
+@main
+struct MochiLog_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
